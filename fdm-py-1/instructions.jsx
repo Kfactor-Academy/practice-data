df80359f8d75a22891fb3bfba5903e567f382e8f
Eliaquim dos Santos <eliaquimdossantos@gmail.com>
2022-03-14 16:44:35 -0300

    Add conteudo e templates

---
+++ fdm-py-1/instructions.jsx
@@ -0,0 +1,3 @@
+<p>Digite no Editor seguinte código e depois clique no botão Executar para ver o resultado:</p>
+<CodeViewer dark>{`print ("Hello World!")`}</CodeViewer>
+<br />
---
+++ fdm-py-1/tips.jsx
@@ -0,0 +1,4 @@
+<ul>
+  <li><code>#Dica1: </code>Lembre-se das aspas <code>" "</code> dentro do <code>( )</code> pois <em>Hello World</em> é um texto e não um número.</li>
+  <li><code>#Dica2: </code>Cuidado com identação incorreta do código. Se o output gerar a mensagem <code>"IndentationError: unexpected indent"</code>, remova espaços antes das linhas dos códigos.</li>
+</ul>
