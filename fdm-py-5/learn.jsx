0f52ab45a9ee54660e0a3af46f8f1fdaf73b4e58
Eliaquim dos Santos <eliaquimdossantos@gmail.com>
2022-03-14 14:50:31 -0300

    Adicionar learn fdm-py-5

---
+++ fdm-py-5/learn.jsx
@@ -0,0 +1,17 @@
+<p>Além do comando <code>print</code> que você já conhece, existem outros comandos básicos em Python que serão úteis para a sua jornada no Data Science.
+  Veja a seguir:</p>
+<p><b>Sum</b>: soma todos os valores em uma lista ou coluna com valores </p>
+<CodeViewer>{`>>> Numeros = [10, 20, 30, 40]
+>>> print (sum (Numeros))
+100`}</CodeViewer>
+<p><b>Max</b>: retorna o maior valor em uma lista</p>
+<CodeViewer>{`>>> Numeros = [10, 20, 30, 40]
+>>> print (sum (Numeros))
+100`}</CodeViewer>
+<p>Veja que introduzimos um novo tipo de dados em Python, as listas.
+  Elas são representadas por uma sequência de dados separados por vírgulas <code>(Valor1, Valor2)</code> e armazenados entre colchetes [ ]:
+</p>
+<CodeViewer>{`>>> Numeros = [10, 20, 30, 40]
+>>> print (sum (Numeros))
+100`}</CodeViewer>
+<p>Nas listas é possível armazenar tipos diferentes de dados, conforme exemplificado acima.</p>
