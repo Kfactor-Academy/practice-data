4b5f7d64cdce57f6a0e286e30e200cc70d2cebfc
Eliaquim dos Santos <34246666+eliaquimdossantos@users.noreply.github.com>
2022-03-14 11:21:51 -0300

    Create lesson.jsx

---
+++ fdm-py-1/lesson.jsx
@@ -0,0 +1,14 @@
+<p>Bem vindo ao mundo do Python!</p>
+<p>Uma forma clássica de testar qualquer linguagem de programação é "printar" na tela <em>Hello World</em>.</p>
+<p>Para fazer isso você pode utilizar o comando <code>print</code> e informar o que se deseja "printar" com parênteses e aspas simples ou duplas, conforme o código abaixo:</p>
+<CodeViewer>{'print("Hello World!")'}</CodeViewer>
+<br />
+<div className="text-center">
+  <p><b>Entenda as funcionalidades da plataforma 🙂</b></p>
+</div>
+<div className="embed-responsive embed-responsive-16by9 video-lesson-width">
+  <video autoPlay={true} playsInline={true} muted loop
+    poster={`${basePathStatic}/onboarding-thumbnail.jpg`}>
+    <source src={`${basePathStatic}/onboarding-video.mp4`} type="video/mp4"></source>
+  </video>
+</div>
