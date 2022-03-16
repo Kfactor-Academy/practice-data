29f7a3a030d00196fc1722188b2507562e77f545
Eliaquim dos Santos <eliaquimdossantos@gmail.com>
2022-03-16 00:03:15 -0300

    Alteração em organização da estrutura

--- fdm-py-1/instructions.jsx
+++
@@ -1,3 +0,0 @@
-<p>Digite no Editor seguinte código e depois clique no botão Executar para ver o resultado:</p>
-<CodeViewer dark>{`print ("Hello World!")`}</CodeViewer>
-<br />
--- fdm-py-6/learn.jsx
+++
@@ -1,4 +0,0 @@
-<br />
-<div className="embed-responsive embed-responsive-16by9 video-lesson-width">
-<iframe src="https://www.youtube.com/embed/yTZtYnWTI-E?rel=0" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
-</div>
